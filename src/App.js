ed98f9345f9a79713db758403b22996b4c5a2caf
Thomas Deschodt <thomas.deschodt@epitech.eu>
2023-07-03 15:16:35 +0200

    [+] Ajout de Fullpage js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import React from 'react';
-import Index from './pages/Index';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { MantineProvider } from '@mantine/core';
+import React, { useEffect, useRef } from 'react';
+import fullpage from 'fullpage.js';
 
 function App() {
+
+  const fullpageRef = React.createRef();
+
+  useEffect(() => {
+    new fullpage(fullpageRef.current, {
+      // Options de configuration de FullPage.js
+    });
+  }, []);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/*" element={<Index />} />
-          <Route path="/home" element={<Index />} />
-        </Routes>
-      </BrowserRouter>
+      <div ref={fullpageRef}>
+      <section className="section" style={{
+        width:"100vw",
+        height:"100vh",
+        backgroundColor:"blue",
+      }}>
+        <h1>Section 1</h1>
+        {/* Contenu de la section 1 */}
+      </section>
+      <section className="section" style={{
+        width:"100vw",
+        height:"100vh",
+        backgroundColor:"blue",
+      }}>
+        <h1>Section 2</h1>
+        {/* Contenu de la section 2 */}
+      </section>
+      <section className="section" style={{
+        width:"100vw",
+        height:"100vh",
+        backgroundColor:"blue",
+      }}>
+        <h1>Section 3</h1>
+        {/* Contenu de la section 3 */}
+      </section>
+      <section className="section">
+        <h1>Section 4</h1>
+        {/* Contenu de la section 4 */}
+      </section>
+      </div>
     </div>
   );
 }
