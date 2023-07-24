ab3d9a35c34eb1406c3bd0e603c95f71c8865bda
mrtn-flmt <mrtn.flmt@gmail.com>
2023-07-24 13:23:06 +0200

    feat: add display image in toturial

--- src/pages/Tutorial.js
+++ src/pages/Tutorial.js
@@ -1,21 +1,87 @@
-import React from 'react';
-import {Flex, Text, Title} from '@mantine/core';
+import React, {useState, useEffect} from 'react';
+import {Flex, Title, Image} from '@mantine/core';
 
-const Container = () => {
+const Container = ({imagePath}) => {
   return (
     <Flex
       style={{
         width: '300px',
         height: '400px',
         backgroundColor: 'white',
-        boxShadow: '2px 5px 9px 0px #0000004F', //droite bas gauche haut - couleur,
+        boxShadow: '2px 5px 9px 0px #0000004F',
         borderRadius: '15px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '50px',
       }}
-    ></Flex>
+    >
+      <Image src={imagePath} alt="Random image" />
+    </Flex>
   );
 };
 
 const Tutorial = () => {
+  const [imageOnePath, setImageOnePath] = useState('');
+  const [imageTwoPath, setImageTwoPath] = useState('');
+  const [imageThreePath, setImageThreePath] = useState('');
+
+  useEffect(() => {
+    const fetchImageOne = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3000/api/files/first.png',
+        );
+        if (response.ok) {
+          const blob = await response.blob();
+          const imageUrl = URL.createObjectURL(blob);
+          setImageOnePath(imageUrl);
+        } else {
+          console.error("Impossible de récupérer l'image.");
+        }
+      } catch (error) {
+        console.error("Erreur lors de la récupération de l'image :", error);
+      }
+    };
+
+    const fetchImageTwo = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3000/api/files/second.png',
+        );
+        if (response.ok) {
+          const blob = await response.blob();
+          const imageUrl = URL.createObjectURL(blob);
+          setImageTwoPath(imageUrl);
+        } else {
+          console.error("Impossible de récupérer l'image.");
+        }
+      } catch (error) {
+        console.error("Erreur lors de la récupération de l'image :", error);
+      }
+    };
+
+    const fetchImageThree = async () => {
+      try {
+        const response = await fetch(
+          'http://localhost:3000/api/files/third_t.png',
+        );
+        if (response.ok) {
+          const blob = await response.blob();
+          const imageUrl = URL.createObjectURL(blob);
+          setImageThreePath(imageUrl);
+        } else {
+          console.error("Impossible de récupérer l'image.");
+        }
+      } catch (error) {
+        console.error("Erreur lors de la récupération de l'image :", error);
+      }
+    };
+
+    fetchImageOne();
+    fetchImageTwo();
+    fetchImageThree();
+  }, []);
+
   return (
     <Flex
       style={{
@@ -52,9 +118,9 @@ const Tutorial = () => {
           marginTop: '5%',
         }}
       >
-        <Container />
-        <Container />
-        <Container />
+        <Container imagePath={imageOnePath} />
+        <Container imagePath={imageTwoPath} />
+        <Container imagePath={imageThreePath} />
       </Flex>
     </Flex>
   );
